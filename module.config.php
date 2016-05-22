5cf99f4302414e35d737ed317f52f4d51694bac5
Md. Shahadat Hossain Khan <razonklnbd@users.noreply.github.com>
2016-05-22 11:28:55 +0600

    Add files via upload

---
+++ module.config.php
@@ -0,0 +1,30 @@
+<?php
+if(!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
+
+return array(
+		'controllers' => array(
+				'invokables' => array(
+						'MinifyJsCss\\Controller\\MinifyJsCss' => 'MinifyJsCss\\Controller\\MinifyJsCssController',
+						#'Wurfl\\Controller\\' => 'Wurfl\\Controller\\Controller',
+				),
+		),
+		'router' => array(
+				'routes' => array(
+						'minify' => array(
+								'type'    => 'segment',
+								'options' => array(
+										'route'    => '/minify[/:action[/:files]]',
+										'defaults' => array(
+												'controller' => 'MinifyJsCss\\Controller\\MinifyJsCss',
+												'action'     => 'index',
+										),
+								),
+						),
+				),
+		),
+		'view_manager' => array(
+				'template_path_stack' => array(
+						'minify' => realpath(__DIR__ . DS.'..'.DS.'view').DS,
+				),
+		),
+);
